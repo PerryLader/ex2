0960da247c8a2efb5dd9ded9924f8f3f8f0fe7fa
dtdt13 <guydtdt@gmail.com>
2022-05-03 17:08:35 +0300

    added the first card functions and finished Player

---
+++ Card.cpp
@@ -0,0 +1,66 @@
+#include "Card.h"
+
+Card::Card(CardType type, const CardStats &stats)
+{
+    m_effect = type;
+
+    m_stats.force = stats.force;
+    m_stats.hpLossOnDefeat = stats.hpLossOnDefeat;
+    m_stats.loot = stats.loot;
+    m_stats.cost = stats.cost;
+    m_stats.heal = stats.heal;
+    m_stats.buff = stats.buff;
+}
+
+void Card::applyEncounter(Player &player) const
+{
+    if (m_effect == CardType::Battle)
+    {
+        if (player.getAttackStrength() >= m_stats.force)
+        {
+            player.addCoins(m_stats.loot);
+        }
+        else
+        {
+            player.dmg(m_stats.hpLossOnDefeat);
+        }
+    }
+    else if (m_effect == CardType::Treasure)
+    {
+        player.addCoins(m_stats.loot);
+    }
+    else if (m_effect == CardType::Heal)
+    {
+        if (player.pay(m_stats.cost))
+        {
+            player.heal(m_stats.heal);
+        }
+    }
+    else if (m_effect == CardType::Buff)
+    {
+        if (player.pay(m_stats.cost))
+        {
+            player.buff(m_stats.buff);
+        }
+    }
+}
+
+void Card::printInfo() const
+{
+    if (m_effect == CardType::Battle)
+    {
+        printBattleCardInfo(m_stats);
+    }
+    else if (m_effect == CardType::Heal)
+    {
+        printHealCardInfo(m_stats);
+    }
+    else if (m_effect == CardType::Buff)
+    {
+        printBuffCardInfo(m_stats);
+    }
+    else if (m_effect == CardType::Treasure)
+    {
+        printTreasureCardInfo(m_stats);
+    }
+}
