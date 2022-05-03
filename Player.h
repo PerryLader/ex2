42bb1902cf3ba1342c2657be38eaf4ca86ca4541
PerryLader <93662497+PerryLader@users.noreply.github.com>
2022-05-03 11:43:55 +0300

    basic white bitch

---
+++ Player.h
@@ -0,0 +1,18 @@
+class Player{
+char * name;
+int level;
+int force;
+int maxHp;
+int curHp;
+int coins;
+
+public:
+Player(const char *playerName,const int maxHp=100,const int force=5);
+
+
+
+
+
+
+
+};
